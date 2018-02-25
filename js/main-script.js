1e9a4fcf21d106c5848769776ac50bafcf86047a
gicontz <61m37.17@gmail.com>
2018-02-25 23:26:45 +0800

    Fix some issues
    - Add student
    - Add admin

--- js/main-script.js
+++ js/main-script.js
@@ -1,13 +1,23 @@
 function filterStudentAttendance (userid, date) {
-  var date = $(".datepicker").val();
-  date = date.replace(/ /g, "%20");
-  var userid = $("[name = 'userid']").val(); 
-      $('#student-attendance').load("inc/attendance-bydate.php?date="+date+"&userid="+userid);
+	var date = $(".datepicker").val();
+	date = date.replace(/ /g, "%20");
+	var userid = $("[name = 'userid']").val(); 
+	$('#student-attendance').load("inc/attendance-bydate.php?date="+date+"&userid="+userid);
 }
 
 $(document).ready(function(){
 	$('.datepicker').datepicker({ dateFormat: 'MM dd, yy' });
-    $('#filter-attendance').click(function(){
-    	filterStudentAttendance();
-    });
+	$('#filter-attendance').click(function(){
+		filterStudentAttendance();
+	});
+
+	$('.breadcrumb li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('.breadcrumb li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	});
 });
---
+++ test.php
@@ -0,0 +1,3 @@
+<?php
+include_once('functions.php');
+$studentClass->addStudent("20123042", "Carlos Miguel", "Balaba", "Embuscado", "", "9123948823", "config.ini");
