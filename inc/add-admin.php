aab17a617d61760b0f494e6e21f0112120b0cdd0
gicontz <gimelcontz@gmail.com>
2018-06-12 15:09:45 +0800

    fix add admin

--- inc/add-admin.php
+++ inc/add-admin.php
@@ -1,5 +1,5 @@
 <?php
-	include('/../functions.php');
+	include('../functions.php');
 	global $adminClass;
 	$conf = "../config.ini";
 	$adminClass->addAdmin($_POST["username"], $_POST['password'], $_POST['firstname'], $_POST['lastname'], $_POST['middlename'], $_POST['extension_name'], $conf);
